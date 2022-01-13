b2648b1deb05b4bf48759f1a20630f09ca4962d8
yon-ninii <701pooh@naver.com>
2022-01-13 18:10:21 +0900

    Project photo

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: 'A.I Study Blog',
-  description: `대학생의 개발 공부 블로그`,
+  description: '대학생의 개발 공부 블로그',
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://yon-ninii.github.io`,
+  siteUrl: 'https://yon-ninii.github.io',
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -14,7 +14,7 @@ module.exports = {
     name: `박용민`,
     bio: {
       role: `대학생`,
-      description: ['A.I에 관심을 가지기 시작한', '컴퓨터 비전', '아직 쪼렙'],
+      description: ['A.I에 관심을 가지기 시작한', '컴퓨터 비전을 배우는', '아직 쪼렙'],
       thumbnail: 'IMG_7105.JPG', // Path to the image in the 'asset' folder
     },
     social: {
@@ -29,28 +29,6 @@ module.exports = {
     timestamps: [
       // =====       [Timestamp Sample and Structure]      =====
       // ===== 🚫 Don't erase this sample (여기 지우지 마세요!) =====
-      {
-        date: '1998.07 ~',
-        activity: '출생',
-        links: {
-          github: '',
-          post: '',
-          googlePlay: '',
-          appStore: '',
-          demo: '',
-        },
-      },
-      {
-        date: '2017.03 ~',
-        activity: '입학',
-        links: {
-          github: 'https://github.com/yon-ninii',
-          post: '',
-          googlePlay: '',
-          appStore: '',
-          demo: '',
-        },
-      },
       {
         date: '2022.01 ~',
         activity: '블로그 오픈',
@@ -72,8 +50,8 @@ module.exports = {
       {
         title: 'A.I 연구실',
         description: '학교에서 좋은 기회로 A.I 컴퓨터 비전 연구실에 들어가게 되었습니다. 이때부터 여러 논문들을 접하고 읽게 되어 그 내용을 정리할 블로그를 만들기로 결정했습니다.',
-        techStack: ['', ''],
-        thumbnailUrl: '',
+        techStack: ['Python', 'PyTorch'],
+        thumbnailUrl: 'Inha.jpg',
         links: {
           post: '',
           github: '',
@@ -84,18 +62,6 @@ module.exports = {
       },
       // ========================================================
       // ========================================================
-      {
-        title: '개발 블로그 테마 개발',
-        description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
     ],
   },
 };
